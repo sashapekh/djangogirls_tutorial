b581ca989895b8d4b6e749f65d4bc803f1ea9d6a
Alexander Pekh <sashapekh@gmail.com>
2015-08-17 13:03:42 +0300

    common changes

---
+++ blog/forms.py
@@ -0,0 +1,9 @@
+__author__ = 'sashapekh'
+from django import forms
+from .models import Post
+
+class PostForm(forms.ModelForm):
+
+    class Meta:
+        model = Post
+        fields = ('title', 'text',)
---
+++ blog/urls.py
@@ -0,0 +1,8 @@
+__author__ = 'sashapekh'
+from django.conf.urls import include, url
+from .import views
+
+urlpatterns = [
+    url(r'^$', views.post_list),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail),
+]
